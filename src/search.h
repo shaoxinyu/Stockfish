823104d49790733a1d5d2ebc0917286cb0c8ccd1
asantirek <18070174+shaoxinyu@users.noreply.github.com>
2023-09-10 19:32:34 +0200

    Update search.h

--- src/search.h
+++ src/search.h
@@ -110,7 +110,24 @@ extern LimitsType Limits;
 void init();
 void clear();
 
+void setLiveBookURL(const std::string &newURL);
+void setLiveBookTimeout(size_t newTimeoutMS);
+
+
+//livebook begin
+#ifdef USE_LIVEBOOK
+void setLiveBookURL(const std::string &newURL);
+void setLiveBookTimeout(size_t newTimeoutMS);
+void set_livebook_retry(int retry);
+void set_livebook_depth(int book_depth);
+#endif
+//livebook end
+
 } // namespace Search
+
+#ifdef USE_LIVEBOOK
+size_t cURL_WriteFunc(void *contents, size_t size, size_t nmemb, std::string *s);
+#endif
 
 } // namespace Stockfish
 
