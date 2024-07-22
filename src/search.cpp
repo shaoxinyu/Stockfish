3d7ff4a5d7cc612a46aa573ab35b77d3b651e430
asantirek <18070174+shaoxinyu@users.noreply.github.com>
2024-07-23 01:52:03 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -248,7 +248,7 @@ void Search::Worker::start_searching() {
               if (g_szRecv.find("move:") != std::string::npos)
               {
                   std::string tmp = g_szRecv.substr(5);
-                  bookMove = Move(rootPos, tmp);
+                  bookMove = UCIEngine::to_move(rootPos, tmp);
                                 livebook_depth_count++;
 }
               }
