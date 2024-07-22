51c595e2e6d33f45acae6c5901836debc2f04b8b
asantirek <18070174+shaoxinyu@users.noreply.github.com>
2024-07-23 00:14:00 +0200

    Update engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -97,6 +97,24 @@ Engine::Engine(std::string path) :
         Tablebases::init(o);
         return std::nullopt;
     });
+
+#ifdef USE_LIVEBOOK
+    options["Live Book"]             << Option(false);
+    options["Live Book URL"]         << Option("http://www.chessdb.cn/cdb.php", [this](const Option&) {
+      Search::setLiveBookURL(o); 
+    });
+    options["Live Book Timeout"]     << Option(5000, 0, 10000, [this](const Option&) {
+      Search::setLiveBookTimeout(o);
+    });
+    options["Live Book Retry"]       << Option(3, 1, 100, [this](const Option&) {
+      Search::set_livebook_retry(o);
+    });
+    options["Live Book Diversity"]   << Option(false);
+    options["Live Book Contribute"]  << Option(false);
+    options["Live Book Depth"]       << Option(100, 1, 100, [this](const Option&) {
+      Search::set_livebook_depth(o);
+    });
+#endif
     options["SyzygyProbeDepth"] << Option(1, 1, 100);
     options["Syzygy50MoveRule"] << Option(true);
     options["SyzygyProbeLimit"] << Option(7, 0, 7);
