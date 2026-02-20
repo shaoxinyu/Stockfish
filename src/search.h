8c51bf228443d0352657ce833f94d61bf42c17bd
asantirek <18070174+shaoxinyu@users.noreply.github.com>
2026-02-20 23:06:10 +0100

    Add livebook configuration functions to search.h

--- src/search.h
+++ src/search.h
@@ -366,6 +366,15 @@ class Worker {
     friend class SearchManager;
 };
 
+//livebook begin
+#ifdef USE_LIVEBOOK
+void setLiveBookURL(const std::string &newURL);
+void setLiveBookTimeout(size_t newTimeoutMS);
+void set_livebook_retry(int retry);
+void set_livebook_depth(int book_depth);
+#endif
+//livebook end
+
 struct ConthistBonus {
     int index;
     int weight;
@@ -373,6 +382,10 @@ struct ConthistBonus {
 
 
 }  // namespace Search
+
+#ifdef USE_LIVEBOOK
+size_t cURL_WriteFunc(void *contents, size_t size, size_t nmemb, std::string *s);
+#endif
 
 }  // namespace Stockfish
 
